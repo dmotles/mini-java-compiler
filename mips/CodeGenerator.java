0be5b35004d217e5fb386bbf08e5c86d9e69d54d
Daniel Motles <seltom.dan@gmail.com>
2012-12-07 16:37:19 -0500

    modified CodeGenerator slightly

--- mips/CodeGenerator.java
+++ mips/CodeGenerator.java
@@ -6,7 +6,31 @@ import visitor.symbol.*;
 import java.util.*;
 
 public class CodeGenerator {
-    public static ArrayList<Instruction> processIR( Quadruple q ) {
+    HashMap<String, ArrayList<Quadruple>> methodIR;
+    Allocator regAllocator;
+    LabelMapping labels;
+
+    public CodeGenerator( HashMap<String, ArrayList<Quadruple>> methodIR ) {
+        this.methodIR = methodIR;
+        regAllocator = new Allocator();
+        labels = new LabelMapping();
+    }
+
+    ArrayList<Instruction> generate() {
+        ArrayList<Instruction> finishedCode = new ArrayList<Instruction>();
+        for( String methodname : methodIR.keySet() ) {
+            ArrayList<Quadruple> IR = methodIR.get( methodname );
+            labels.add( methodName );
+            finishedCode.add( labels.get(methodname) );
+            Allocator.allocateRegisters( IR );
+            for( Quadruple quad : IR ) {
+                finishedCode.addAll( processQuad( quad ) );
+            }
+        }
+        return finishedCode;
+    }
+
+    private ArrayList<Instruction> processQuad( Quadruple q ) {
         ArrayList<Instruction> ilist = null;
         switch( q.quadType() ) {
             case Quadruple.ARRAYASSIGNMENTQUADRUPLE:
@@ -21,7 +45,7 @@ public class CodeGenerator {
                 if( firstArg instanceof Constant ) {
                     LI li = new LI();
                     li.setImmediate( firstArg );
-                    li.setRS( Allocator.parseRegister( result.getName().toString() ) );
+                    li.setRS( regAllocator.getRegister( result );
                     ilist.add(li);
                 }
                 break;
